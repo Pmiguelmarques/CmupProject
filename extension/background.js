e886af5bfc978f7aa5e61cb6f7f31e0a3da4e608
José Frias <josefrias99@ua.pt>
2021-07-09 15:21:54 +0100

    reconnect browser socket when coordinator is stopped

--- extension/background.js
+++ extension/background.js
@@ -1,31 +1,36 @@
-const socket = new WebSocket('ws://localhost:8080');
-
-socket.addEventListener('message', function(event){
-    console.log(event.data);
-})
-
-socket.addEventListener('open', function(event){
+function initialize(){
     
-    socket.send('Connected');
+    const socket = new WebSocket('ws://localhost:8080');
 
-    chrome.tabs.onActivated.addListener(tab => {
-        chrome.tabs.get(tab.tabId, current_tab_info => {
-            socket.send(current_tab_info.url);
-            socket.send(current_tab_info.title);
+    socket.addEventListener('message', function(event){
+        console.log(event.data);
+    })
+
+    socket.addEventListener('open', function(event){
+        
+        socket.send('Connected');
+
+        chrome.tabs.onActivated.addListener(tab => {
+            chrome.tabs.get(tab.tabId, current_tab_info => {
+                socket.send(current_tab_info.url);
+                socket.send(current_tab_info.title);
+            })
+
         })
 
+        chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
+            socket.send(tab.url);
+            socket.send(tab.title);
+        })
     })
 
-    chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
-        socket.send(tab.url);
-        socket.send(tab.title);
+
+    socket.addEventListener('close', function(event){
+        setTimeout(function() {
+            initialize();
+        }, 60);
     })
-})
 
+}
 
-socket.addEventListener('close', function(event){
-    console.log("Trying to reconnect");
-    setTimeout(function() {
-        connect();
-      }, 1000);
-})
+initialize();
