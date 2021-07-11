a654e15b39bf82050bda205d5614d183b24022ad
José Frias <josefrias99@ua.pt>
2021-07-11 15:56:58 +0100

    specify requirements to run the application

--- coordinator.py
+++ coordinator.py
@@ -1,11 +1,8 @@
-from typing import Tuple
-from google.protobuf import message
 import websockets
 import asyncio 
 import socket
 import threading
 import time
-import operator
 from tensorflow.keras.models import load_model
 import numpy as np
 from tkinter import *
